39b375c44a1eff67aea64af87d816dd37993a87c
Justin <justin.halse@gmail.com>
2025-05-26 23:17:02 +0200

    rough updates

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # NotifyMe
+
+## Commands
+
+### Local (Node)
+
+- > npm build
+- > node bot.js
+
+### Docker
+
+- > docker build -t notify-me .
+- > docker run -d --name notify-me-container-2 -p 3000:3000 notify-me
+  
+Not quite but close
+
--- bot.js
+++ bot.js
@@ -1,11 +1,20 @@
 import { Client, GatewayIntentBits, NewsChannel } from "discord.js";
+import fetch from 'node-fetch';
 import "dotenv/config";
 
 console.log("Starting...");
 
+let connectedUsers = [];
+
 const token = process.env.discord_token;
 const apikey = process.env.call_me_bot_api_key;
 const phoneNumber = process.env.cell_number;
+
+const accessToken = process.env.facebook_access_token; // Get from Meta Developer Console
+const phoneNumberId = process.env.facebook_phone_number_id; // Assigned in WhatsApp Business API setup
+const recipientPhone = process.env.cell_number; // Format: 'whatsapp:+1234567890'
+
+
 const client = new Client({
   intents: [
     GatewayIntentBits.GuildVoiceStates,
@@ -19,22 +28,27 @@ client.login(token);
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`---------------------------------------`);
 });
 
 client.on("voiceStateUpdate", (oldState, newState) => {
   try {
+    
+    console.log(`OldState: ${JSON.stringify(oldState)}`);
+    console.log(`NewState: ${JSON.stringify(newState)}`);
+    
+    console.log(newState.guild.name);
+    console.log(oldState.guild.name);
+    
     if (newState.channel && newState.channel.members) {
       const connectedUsers = [newState.guild.name];
+      
       newState.channel.members.forEach((member) => {
         connectedUsers.push(member.user.username);
       });
 
       if (connectedUsers) {
-        QueueWhatsAppMessage(
-          `User connected to ${connectedUsers.at(
-            0
-          )} | Connected : ${GetAllUserNames(connectedUsers)}`
-        );
+        sendWhatsAppMessage(`${GetAllUserNames(connectedUsers)}`,  connectedUsers.at(0), connectedUsers.at(0)).then(r => {});
       }
     }
 
@@ -45,11 +59,7 @@ client.on("voiceStateUpdate", (oldState, newState) => {
         connectedUsers.push(member.user.username);
       });
       if (connectedUsers) {
-        QueueWhatsAppMessage(
-          `User disconnected to ${connectedUsers.at(
-            0
-          )} | Connected: ${GetAllUserNames(connectedUsers)}`
-        );
+        sendWhatsAppMessage(`${GetAllUserNames(connectedUsers)}`,  connectedUsers.at(0), connectedUsers.at(0)).then(r => {});
       }
     }
   } catch (err) {
@@ -69,22 +79,62 @@ function GetAllUserNames(connectedUsers) {
   return names;
 }
 
-function QueueWhatsAppMessage(msg) {
-  try {
-    const timeStamp = new Date().toTimeString();
-    console.log(`${timeStamp} | Queueing WhatsApp message to send: ${msg}`);
-    const encodedMessage = encodeURIComponent(msg);
 
-    fetch(
-      `https://api.callmebot.com/whatsapp.php?phone=${phoneNumber}&text=${encodedMessage}&apikey=${apikey}`
-    )
-      .then((result) => {
-        // console.log(result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  } catch (err) {
-    console.log(err);
-  }
+async function sendWhatsAppMessage(users, channelName, server) {
+  console.log(`Users: ${users}`);
+  console.log(`Channel Name: ${channelName}`);
+  console.log(`Server:  ${server}`);
+
+  const timeStamp = new Date().toTimeString();
+  const url = `https://graph.facebook.com/v22.0/${phoneNumberId}/messages`;
+
+  const messageData = {
+    messaging_product: 'whatsapp',
+    to: recipientPhone,
+    type: 'template',
+    template: { name: "user_connected", 
+      language: {
+        code : "en_ZA"}, 
+      components: [{
+          type: "body",
+          parameters: [
+            {
+              type: "text",
+              parameter_name: "users",
+              text: users
+            },
+            {
+              type: "text",
+              parameter_name: "channel_name",
+              text: channelName
+            }
+          ]
+      },
+        {
+          type: "header",
+          parameters: [
+            {
+              text: server,
+              type: "text",
+              parameter_name: "server",
+            }  
+          ]
+        }] }
+  };
+
+  console.log(`Message Data: ${JSON.stringify(messageData)}`);
+  
+  
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(messageData)
+  });
+
+  const result = await response.json();
+  console.log(result);
 }
+
